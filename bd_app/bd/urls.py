f54ed47b6a21be2a71a5b947a077d565ca384fd9
ksenia02romanova <125283413+ksenia02romanova@users.noreply.github.com>
2025-05-28 19:35:20 +0300

    Add files via upload

---
+++ bd_app/bd/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.bd_watching,name= 'bd_watching'),
+    path("create_order", views.create_order,name= 'create_order'),
+    path("catalog", views.catalog,name= 'catalog')
+] 
